2fa5f8cf5f10d77aa14e4bbc5776f86c3dbff541
xAzoom <morenequs@gmail.com>
2019-04-18 22:27:46 +0200

    fix refreshUser in UserProvider add isEqualTo in user entity add logout_on_user_change: true flag in security.yaml

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,9 +2,10 @@
 
 namespace AppBundle\Entity;
 
+use Symfony\Component\Security\Core\User\EquatableInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class User implements UserInterface
+class User implements UserInterface, EquatableInterface
 {
     private $email;
 
@@ -77,6 +78,39 @@ class User implements UserInterface
      */
     public function eraseCredentials()
     {
-        // TODO: Implement eraseCredentials() method.
+
+    }
+
+    /**
+     * The equality comparison should neither be done by referential equality
+     * nor by comparing identities (i.e. getId() === getId()).
+     *
+     * However, you do not need to compare every attribute, but only those that
+     * are relevant for assessing whether re-authentication is required.
+     *
+     * Also implementation should consider that $user instance may implement
+     * the extended user interface `AdvancedUserInterface`.
+     *
+     * @return bool
+     */
+    public function isEqualTo(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if ($this->email != $user->getUsername()) {
+            return false;
+        }
+
+        if ($this->password != $user->getPassword()) {
+            return false;
+        }
+
+        if ($this->roles != $user->getRoles()) {
+            return false;
+        }
+
+        return true;
     }
 }
