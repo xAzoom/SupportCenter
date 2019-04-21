978543019f20b1a9145e196a22ec56b6d9206201
xAzoom <morenequs@gmail.com>
2019-04-21 23:43:49 +0200

    add createTable method to DbManager

--- src/DbBundle/Services/DbManager.php
+++ src/DbBundle/Services/DbManager.php
@@ -3,6 +3,7 @@
 namespace DbBundle\Services;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Schema;
 
 class DbManager implements IDbManager
 {
@@ -22,7 +23,7 @@ class DbManager implements IDbManager
         $qB->select($columns)->from($table);
 
         foreach ($by as $key => $value) {
-            $qB->andWhere($key.' = :'.$key);
+            $qB->andWhere($key . ' = :' . $key);
             $qB->setParameter($key, $value);
         }
 
@@ -53,5 +54,65 @@ class DbManager implements IDbManager
         $qB->values($columns);
 
         $qB->execute();
+    }
+
+    public function createTable($name, array $columns, array $primary_keys = [], array $unique_keys = [])
+    {
+        if (!$this->areValidDataToCreateTable($name, $columns, $primary_keys, $unique_keys)) {
+            throw new \Exception('Invalid data to createTable');
+        }
+
+        if ($this->dbConnection->getSchemaManager()->tablesExist([$name])) {
+            throw new \Exception(sprintf('Table with name %s already exist', $name));
+        }
+
+        $schema = new Schema();
+        $table = $schema->createTable($name);
+
+        foreach ($columns as $column) {
+            $table->addColumn($column[0], $column[1], isset($column[2]) ? $column[2] : []);
+        }
+
+
+        if(!empty($primary_keys)) {
+            $table->setPrimaryKey($primary_keys);
+        }
+
+        if(!empty($unique_keys)) {
+            $table->addUniqueIndex($unique_keys);
+        }
+
+        $this->dbConnection->getSchemaManager()->createTable($table);
+    }
+
+    private function areValidDataToCreateTable($name, array $columns, array $primary_keys, array $unique_keys)
+    {
+        if (!is_string($name)) {
+            return false;
+        }
+
+        if (!is_array($columns) || empty($columns)) {
+            return false;
+        }
+
+        foreach ($columns as $column) {
+            if (\count($column) < 2) {
+                return false;
+            }
+
+            if (isset($column[2]) && !is_array($column[2])) {
+                return false;
+            }
+        }
+
+        if (!is_array($primary_keys)) {
+            return false;
+        }
+
+        if (!is_array($unique_keys)) {
+            return false;
+        }
+
+        return true;
     }
 }
--- src/DbBundle/Services/IDbManager.php
+++ src/DbBundle/Services/IDbManager.php
@@ -7,4 +7,5 @@ interface IDbManager
     public function findOneBy($table, array $columns, array $by);
     public function findBy($table, array $columns, array $by);
     public function insert($table, array $values);
+    public function createTable($name, array $columns, array $primary_keys = [], array $unique_keys = []);
 }
